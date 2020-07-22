e9d4fc0357ef5a60b3668715fa39928992077575
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-22 14:54:42 -0600

    set Login and Setup App.js

--- app-4/src/App.js
+++ app-4/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Components/Login';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super()
+  }
+
+render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ='App'>
+      <Login />
     </div>
-  );
+  )
+}
+
 }
 
 export default App;
