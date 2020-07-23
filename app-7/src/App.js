6d18b3d0f6811716aedb17f622059f49960c2b45
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-22 18:31:39 -0600

    couldn't find error so C&P

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,33 +1,33 @@
-
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import List from './Components/List'
-import NewTask from './Components/NewTask'
-// import Todo from './Components/Todo'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NewTask from "./Components/NewTask";
+import List from "./Components/List";
 
 class App extends Component {
-  constructor(){
-    super ()
+  constructor() {
+    super();
 
     this.state = {
-      list: [ ]
+      list: []
     };
 
-
-
+    this.handleAddTask = this.handleAddTask.bind(this);
   }
 
-  render(){
+  handleAddTask(task) {
+    this.setState({ list: [...this.state.list, task] });
+  }
+
+  render() {
     return (
-      <div className='App'>
-        <h1>GET DONE STUFF</h1>
+      <div className="App">
+        <h1>My to-do list:</h1>
         <NewTask add={this.handleAddTask} />
+        <List tasks={this.state.list} />
       </div>
-    )
+    );
   }
 }
-
-
 
 export default App;
--- app-7/src/Components/NewTask.js
+++ app-7/src/Components/NewTask.js
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NewTask extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super();
 
-        this.state = {
-            input: ''
-        };
+    this.state = {
+      input: ""
+    };
 
+    this.handleAdd = this.handleAdd.bind(this);
+  }
 
+  handleInputChange(value) {
+    //   console.log(this)
+    this.setState({ input: value });
+  }
 
-    }
+  handleAdd() {
+    this.props.add(this.state.input);
+    this.setState({ input: "" });
+  }
 
-    handleInputChange(value){
-        this.setState({ input: value })
-    }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.input}
+          placeholder="Enter new task"
+          onChange={e => this.handleInputChange(e.target.value)}
+        />
 
-    handleAdd(){
-       this.props.add(this.state.input);
-       this.setState({ input: '' })
-    }
-
-    render(){
-        return(
-            <div>
-                <input 
-                    value={this.state.input}
-                    placeholder='Input things thats most get done'
-                    onChange={event => this.handleInputChange(event.target.value)}
-                    />
-                    <button 
-                    onClick={this.handleAdd}>
-                        ADD
-                    </button>
-            </div>
-        )
-    }
-
+        <button onClick={this.handleAdd}>Add</button>
+      </div>
+    );
+  }
 }
 
-export default NewTask
+export default NewTask;
